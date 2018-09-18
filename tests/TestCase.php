adc819e98ebab0fb5c7d4ef33709285b42612d8c
Peter Moser <prog.codelair@googlemail.com>
2018-09-18 11:56:18 +0000

    Addes sing in helper methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,18 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function signIn(array $attributes = [], $guard = 'web')
+    {
+        $user = factory(\App\User::class)->create($attributes);
+
+        $this->be($user, $guard);
+    }
+    
+    public function apiSignIn(array $attributes = [])
+    {
+        $user = factory(\App\User::class)->create($attributes);
+
+        $this->be($user, 'api');
+    }
 }
